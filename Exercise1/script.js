57fb37340a85d6757ecfadca9991a7327ffa8b2b
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-03-12 13:28:26 +0100

    Separated script and style from html

---
+++ Exercise1/script.js
@@ -0,0 +1,5 @@
+var canvas = document.querySelector('canvas');
+var gl = canvas.getContext('webgl');
+
+gl.clearColor(Math.random(0.0, 1.0), Math.random(0.0, 1.0), Math.random(0.0, 1.0), 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
